529983f7552fc108875b9149704a6c252bf3ac3d
Jani Yli-Paavola <jylipaa@iki.fi>
2017-08-12 13:08:46 +0300

    Created non-op entry point as Composer vendor bin

---
+++ src/Releasy/Releasy.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BelowAverage\Releasy;
+
+/**
+ * @package BelowAverage.Releasy
+ * @version 0.1.0   2017-08-12
+ * @author  Jani Yli-Paavola
+ * @license MIT
+ */
+class Releasy {
+    
+    /**
+     * Main method
+     * 
+     * @param array $arg
+     */
+    public function run($arg = []) {
+
+    }
+    
+    protected function major() {}
+    protected function minor() {}
+    protected function patch() {}
+    protected function preRelease($preRelease) {}
+    protected function build($build) {}
+    
+}
